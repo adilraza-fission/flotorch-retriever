9915e4f5a3b05d7b64da57dbae60ef3d61145a70
DESKTOP-C010PSV\FL_LPT-470 <adil.raza@fissionlabs.com>
2025-03-04 12:42:08 +0530

    sagemaker, metrics, bedrock knowledgebase

--- fargate/retriever_processor.py
+++ fargate/retriever_processor.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 import json
+from typing import Dict, List, Optional, Union
+import uuid
 from embedding.embedding_registry import embedding_registry
 from embedding.guardrails.guardrails_embedding import GuardrailsEmbedding
 from fargate.base_task_processor import BaseFargateTaskProcessor
@@ -11,9 +14,11 @@ from config.env_config_provider import EnvConfigProvider
 from reader.json_reader import JSONReader
 from rerank.rerank import BedrockReranker
 from retriever.retriever import Retriever
+from storage.db.dynamodb import DynamoDB
 from storage.db.vector.guardrails_vector_storage import GuardRailsVectorStorage
 from storage.db.vector.open_search import OpenSearchClient
 from inferencer.bedrock_inferencer import BedrockInferencer
+from storage.db.vector.vector_storage_factory import VectorStorageFactory
 from storage.storage_provider_factory import StorageProviderFactory
 
 from embedding.titanv2_embedding import TitanV2Embedding
@@ -96,8 +101,6 @@ class RetrieverProcessor(BaseFargateTaskProcessor):
 
             logger.info(f"Into retriever processor. Processing event: {json.dumps(exp_config_data)}")
 
-            index_id = exp_config_data.get("index_id")
-
             gt_data = exp_config_data.get("gt_data")
             storage = StorageProviderFactory.create_storage_provider(gt_data)
             gt_data_path = storage.get_path(gt_data)
@@ -112,21 +115,22 @@ class RetrieverProcessor(BaseFargateTaskProcessor):
             if exp_config_data.get("enable_guardrails", False):
                 base_guardrails = BedrockGuardrail(exp_config_data.get("guardrail_id", ""), exp_config_data.get("guardrail_version", 0))
             
-            # if exp_config_data.get("enable_guardrails", False) and exp_config_data.get("enable_prompt_guardrails", False):
-            #     embedding = GuardrailsEmbedding(embedding, base_guardrails)
             
-            open_search_client = OpenSearchClient(
-                config.get_opensearch_host(), 
-                config.get_opensearch_port(),
-                config.get_opensearch_username(), 
-                config.get_opensearch_password(),
-                index_id,
-                embedder=embedding
+            vector_storage = VectorStorageFactory.create_vector_storage(
+                use_bedrock_kb=exp_config_data.get("bedrock_knowledge_base", False),
+                embedding=embedding,
+                opensearch_host=config.get_opensearch_host(),
+                opensearch_port=config.get_opensearch_port(),
+                opensearch_username=config.get_opensearch_username(),
+                opensearch_password=config.get_opensearch_password(),
+                index_id=exp_config_data.get("index_id"),
+                knowledge_base_id=exp_config_data.get("kb_data"),
+                aws_region=exp_config_data.get("aws_region")
             )
 
             if exp_config_data.get("enable_guardrails", False) and exp_config_data.get("enable_prompt_guardrails", False):
-                open_search_client = GuardRailsVectorStorage(
-                    open_search_client, 
+                vector_storage = GuardRailsVectorStorage(
+                    vector_storage, 
                     base_guardrails,
                     exp_config_data.get("enable_prompt_guardrails", False),
                     exp_config_data.get("enable_context_guardrails", False)
@@ -136,7 +140,7 @@ class RetrieverProcessor(BaseFargateTaskProcessor):
                 if exp_config_data.get("rerank_model_id").lower() != "none" \
                 else None
             
-            inferencer = InferencerProviderFactory(
+            inferencer = InferencerProviderFactory.create_inferencer_provider(
                 exp_config_data.get("retrieval_service"),
                 exp_config_data.get("retrieval_model"), 
                 exp_config_data.get("aws_region"), 
@@ -148,17 +152,88 @@ class RetrieverProcessor(BaseFargateTaskProcessor):
             if exp_config_data.get("enable_guardrails", False) and exp_config_data.get("enable_response_guardrails", False):
                 inferencer = GuardRailsInferencer(inferencer, base_guardrails)
 
-            retriever = Retriever(json_reader, embedding, open_search_client, inferencer, reranker)
+            retriever = Retriever(json_reader, embedding, vector_storage, inferencer, reranker)
             hierarchical = exp_config_data.get("chunking_strategy") == 'hierarchical'
-            retriever.retrieve(
+            result = retriever.retrieve(
                 gt_data_path, 
                 "What is the patient's name?",
-                exp_config_data.get("knn_num"), 
+                int(exp_config_data.get("knn_num")), 
                 hierarchical
             )
+
+            # metrics
+            batch_items = []
+            for item in result:
+                metrics = create_metrics(
+                    exp_config_data.get("execution_id"),
+                    exp_config_data.get("experiment_id"),
+                    item.question,
+                    item.gt_answer,
+                    item.answer,
+                    item.reference_contexts,
+                    item.query_metadata,
+                    item.answer_metadata,
+                    item.guardrails_input_assessment,
+                    item.guardrails_context_assessment,
+                    item.guardrails_output_assessment,
+                    exp_config_data.get("guardrail_id", ""),
+                    item.guardrails_blocked
+                )
+                batch_items.append(metrics)
+                if len(batch_items) >= 25:
+                    write_batch_to_metrics_dynamodb(batch_items)
+                    batch_items = []
+
+            if len(batch_items) > 0:
+                write_batch_to_metrics_dynamodb(batch_items)
+
             
             output = {"status": "success", "message": "Retriever completed successfully."}
             self.send_task_success(output)
         except Exception as e:
             logger.error(f"Error during retriever process: {str(e)}")
             self.send_task_failure(str(e))
+
+
+
+# metrics: to be removed later
+def create_metrics(
+    execution_id,
+    experiment_id,
+    question: str,
+    gt_answer: str,
+    answer: str,
+    reference_contexts: List[str],
+    query_metadata: Dict[str, int],
+    answer_metadata: Dict[str, int],
+    guardrail_input_assessment: Optional[Union[List[Dict], Dict]] = None,
+    guardrail_context_assessment: Optional[Union[List[Dict], Dict]] = None,
+    guardrail_output_assessment: Optional[Union[List[Dict], Dict]] = None,
+    guardrail_id: Optional[str] = None,
+    guardrail_blocked: Optional[str] = None
+):
+    metrics = {
+        "id": str(uuid.uuid4()),
+        "timestamp": datetime.now().isoformat(),
+        "execution_id": execution_id,
+        "experiment_id": experiment_id,
+        "question": question,
+        "gt_answer": gt_answer,
+        "answer": answer,
+        "reference_contexts": reference_contexts,
+        "query_metadata": query_metadata,
+        "answer_metadata": answer_metadata,
+        "guardrail_input_assessment": guardrail_input_assessment,
+        "guardrail_context_assessment": guardrail_context_assessment,
+        "guardrail_output_assessment": guardrail_output_assessment,
+        "guardrail_id": guardrail_id,
+        "guardrail_blocked": guardrail_blocked
+    }
+    return metrics
+
+
+def write_batch_to_metrics_dynamodb(batch_items: List[Dict]) -> None:
+    """Write a batch of items to DynamoDB."""
+    logger.info(f"Writing batch of {len(batch_items)} items to DynamoDB")
+    db = DynamoDB(config.get_experiment_question_metrics_table())
+    db.bulk_write(batch_items)
